c1e7ec76fe3213d4148a9676a4a5344667092abc
Offbeat <info@offbeatsoftwaresolutions.com>
2015-11-04 15:47:44 +0530

    Add employeeID

--- ISEE/Models/jstreenode.cs
+++ ISEE/Models/jstreenode.cs
@@ -11,6 +11,7 @@ namespace ISEE.Models
         public string parent { get; set; }
         public string text { get; set; }
         public string icon { get; set; }
+        public int employeeId { get; set; }
         public jstreenode()
         {
             parent = "#";

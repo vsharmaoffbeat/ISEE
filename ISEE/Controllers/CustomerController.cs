d2ba0f39945df1cb21686830e429fd124bf9ef6d
Offbeat <info@offbeatsoftwaresolutions.com>
2015-11-18 10:02:05 +0530

    Update customer tab

---
+++ ISEE/Controllers/CustomerController.cs
@@ -0,0 +1,144 @@
+﻿using ISEEDataModel.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ISEE.Controllers
+{
+    public class CustomerController : Controller
+    {
+        //
+        // GET: /Customer/
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Customer()
+        {
+            return View();
+        }
+        public string GetNullableValues(string value)
+        {
+            if (string.IsNullOrEmpty(value.Trim()))
+                return null;
+            return value;
+
+        }
+        //{ state: state, city: city, street: street, building: building, custNumber: $('#custNumber').text().trim(), firstName: $('#custName').text().trim(), lastName: $('#custCompany').text().trim(), phone: $('#custPhone').text().trim(), phone1: $('#custPhone1').text().trim(), isActive: $('#isActive').is(':checked') }//
+        public JsonResult GetCustomerSarch(int state, int city, int street, string building, string custNumber, string firstName, string lastName, string phone, string phone1, bool isActive)
+        {
+
+            building = GetNullableValues(building);
+            custNumber = GetNullableValues(custNumber);
+            firstName = GetNullableValues(firstName);
+            lastName = GetNullableValues(lastName);
+            phone = GetNullableValues(phone);
+            phone1 = GetNullableValues(phone1);
+
+
+
+            using (ISEEEntities context = new ISEEEntities())
+            {
+
+                var ss = (from cc in context.Customers.Include("Building").Include("Building.Street").Include("Building.Street.City").Include("Building.Street.City.State")
+                          join s in context.States on cc.Building.StateCode equals s.StateCode
+                          join c in context.Cities on cc.Building.CityCode equals c.CityCode
+                          join st in context.Streets on cc.Building.StreetCode equals st.StreetCode
+                          where cc.Factory == ISEE.Common.SessionManegment.SessionManagement.FactoryID
+                     &&
+                      (state != 0 ? cc.Building.StateCode == state : cc.Building.StateCode == null) &&
+      cc.Building.StateCode == (state == 0 ? cc.Building.StateCode : state) &&
+      cc.Building.CityCode == (city == 0 ? cc.Building.CityCode : city) &&
+      cc.Building.StreetCode == (street == 0 ? cc.Building.StreetCode : street) &&
+      cc.Building.Number.Contains(building == null ? cc.Building.Number : building) &&
+      cc.CustomerNumber.CompareTo(custNumber == null ? cc.CustomerNumber : custNumber) == 0
+
+
+                 && cc.FirstName.Contains(firstName == null ? cc.FirstName : firstName) &&
+      (cc.LastName == null || cc.LastName.Contains(lastName == null ? cc.LastName : lastName)) &&
+      (cc.AreaPhone1 == null || cc.AreaPhone1.Contains(phone == null ? cc.AreaPhone1 : phone)) &&
+      (cc.Phone1 == null || cc.Phone1.Contains(phone1 == null ? cc.Phone1 : phone1)) &&
+      (isActive ? (cc.EndDate == null || (cc.EndDate != null && cc.EndDate >= DateTime.Now)) : (cc.EndDate != null && cc.EndDate < DateTime.Now))
+                          select new
+    {
+        CustomerId = cc.CustomerId,
+        CustomerNumber = cc.CustomerNumber,
+        FirstName = cc.FirstName,
+        LastName = cc.LastName,
+        Floor = cc.Floor,
+        Apartment = cc.Apartment,
+        AreaPhone1 = cc.AreaPhone1,
+        Phone1 = cc.Phone1,
+        AreaPhone2 = cc.AreaPhone2,
+        Phone2 = cc.Phone2,
+        AreaFax = cc.AreaFax,
+        Fax = cc.Fax,
+        Mail = cc.Mail,
+        CustomerRemark1 = cc.CustomerRemark1,
+        CustomerRemark2 = cc.CustomerRemark2,
+        VisitInterval = cc.VisitInterval,
+        NextVisit = cc.NextVisit,
+        VisitDate = cc.VisitDate,
+        VisitTime = cc.VisitTime,
+        EndDate = cc.EndDate,
+        Lat = cc.Building.Lat,
+        Long = cc.Building.Long,
+        ZipCode = cc.Building.ZipCode,
+        StateName = cc.Building.StateCode,
+        IS = c.CityDesc
+    }).ToList();
+
+                //var ccccc= (from ss in context.States 
+
+
+                var results = context.Customers.Include("Building").Include("Building.Street").Include("Building.Street.City").Include("Building.Street.City.State").Where(x => x.Factory == ISEE.Common.SessionManegment.SessionManagement.FactoryID &&
+                     (state != 0 ? x.Building.StateCode == state : x.Building.StateCode == null) &&
+     x.Building.StateCode == (state == 0 ? x.Building.StateCode : state) &&
+     x.Building.CityCode == (city == 0 ? x.Building.CityCode : city) &&
+     x.Building.StreetCode == (street == 0 ? x.Building.StreetCode : street) &&
+     x.Building.Number.Contains(building == null ? x.Building.Number : building) &&
+     x.CustomerNumber.CompareTo(custNumber == null ? x.CustomerNumber : custNumber) == 0
+
+
+                && x.FirstName.Contains(firstName == null ? x.FirstName : firstName) &&
+     (x.LastName == null || x.LastName.Contains(lastName == null ? x.LastName : lastName)) &&
+     (x.AreaPhone1 == null || x.AreaPhone1.Contains(phone == null ? x.AreaPhone1 : phone)) &&
+     (x.Phone1 == null || x.Phone1.Contains(phone1 == null ? x.Phone1 : phone1)) &&
+     (isActive ? (x.EndDate == null || (x.EndDate != null && x.EndDate >= DateTime.Now)) : (x.EndDate != null && x.EndDate < DateTime.Now))).ToList().Select(x => new
+     {
+         CustomerId = x.CustomerId,
+         CustomerNumber = x.CustomerNumber,
+         FirstName = x.FirstName,
+         LastName = x.LastName,
+         Floor = x.Floor,
+         Apartment = x.Apartment,
+         AreaPhone1 = x.AreaPhone1,
+         Phone1 = x.Phone1,
+         AreaPhone2 = x.AreaPhone2,
+         Phone2 = x.Phone2,
+         AreaFax = x.AreaFax,
+         Fax = x.Fax,
+         Mail = x.Mail,
+         CustomerRemark1 = x.CustomerRemark1,
+         CustomerRemark2 = x.CustomerRemark2,
+         VisitInterval = x.VisitInterval,
+         NextVisit = x.NextVisit,
+         VisitDate = x.VisitDate,
+         VisitTime = x.VisitTime,
+         EndDate = x.EndDate,
+         Lat = x.Building.Lat,
+         Long = x.Building.Long,
+         ZipCode = x.Building.ZipCode,
+         StateName = x.Building.StateCode,
+
+     }).ToList();
+
+                return new JsonResult { Data = results, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+            }
+        }
+
+
+    }
+}
---
+++ ISEE/Scripts/Custom/Customer.js
@@ -0,0 +1,239 @@
+﻿var stateNames = [];
+var stateIds = [];
+$(document).ready(function () {
+    GetStaresByFactoryID();
+});
+
+
+
+
+
+function GetStaresByFactoryID() {
+    stateNames = [];
+    stateIds = [];
+    $.ajax({
+        type: "POST",
+        url: "/Data/GetStaresByFactoryID",
+        success: function (response) {
+            if (response.length == 1 && response[0].CountryDescEng == null) {
+
+                stateNames.push('');
+                stateIds.push(response[0].CountryCode);
+                $('#custState').val('');
+                GetCitysByState();
+            }
+            else if (response != null && response.length > 0 && response[0].CityDesc != null) {
+                $(response).each(function () {
+                    stateNames.push(this.CountryDescEng);
+                    stateIds.push(this.CountryCode);
+                });
+            }
+            $("#custState").autocomplete({
+                source: stateNames,
+            });
+        },
+        //error: function (xhr, ajaxOptions, thrownError) { alert(xhr.responseText); }
+    });
+}
+
+//Get cities based on state
+var abliableDataForCityesName = [];
+var abliableDataForCityesIds = [];
+function GetCitysByState() {
+    if (stateIds[stateNames.indexOf($('#custState').val())] == undefined)
+        return false;
+
+    abliableDataForCityesName = [];
+    abliableDataForCityesIds = [];
+    $.ajax({
+        type: "POST",
+        url: "/Data/GetCitysByState",
+        data: { stateID: stateIds[stateNames.indexOf($('#custState').val())] },
+        dataType: "json",
+        success: function (response) {
+            if (response.length == 1 && response[0].CityDesc == null) {
+                abliableDataForCityesName.push('');
+                abliableDataForCityesIds.push(response[0].CityCode);
+                $('#custCity').val('');
+                GetStreetByCity();
+            }
+            else if (response != null && response.length > 0 && response[0].CityDesc != null) {
+                $(response).each(function () {
+                    abliableDataForCityesName.push(this.CityDesc);
+                    abliableDataForCityesIds.push(this.CityCode);
+                });
+            }
+            $("#custCity").autocomplete({
+                source: abliableDataForCityesName,
+            });
+        },
+        //error: function (xhr, ajaxOptions, thrownError) { alert(xhr.responseText); }
+    });
+}
+
+//Get street based on state and city
+var abliableDataForStreetName = [];
+var abliableDataForStreetId = [];
+function GetStreetByCity() {
+    if (stateIds[stateNames.indexOf($('#custState').val())] == undefined || abliableDataForCityesIds[abliableDataForCityesName.indexOf($('#custCity').val())] == undefined)
+        return false;
+    $.ajax({
+        type: "POST",
+        url: "/Data/GetStreetByCity",
+        data: { stateID: stateIds[stateNames.indexOf($('#custState').val())], cityID: abliableDataForCityesIds[abliableDataForCityesName.indexOf($('#custCity').val())] },
+        dataType: "json",
+        success: function (response) {
+            if (response.length == 1 && response[0].Streetdesc == null) {
+
+                abliableDataForStreetName.push('');
+                abliableDataForStreetId.push(response[0].StreetCode);
+
+                $('#custStreet').val('');
+                GetBuildingsByCity();
+            }
+            else if (response != null && response.length > 0 && response[0].Streetdesc != null) {
+                $(response).each(function () {
+                    abliableDataForStreetName.push(this.Streetdesc);
+                    abliableDataForStreetId.push(this.StreetCode);
+                });
+            }
+            $("#custStreet").autocomplete({
+                source: abliableDataForStreetName,
+            });
+        },
+        //error: function (xhr, ajaxOptions, thrownError) { alert(xhr.responseText); }
+    });
+}
+
+//Get Building  based on state and city and street
+var abliableDataForBuildingNumber = [];
+var abliableDataForBuildingId = [];
+var abliableDataForBuildingLat = [];
+var abliableDataForBuildingLong = [];
+
+function GetBuildingsByCity() {
+    abliableDataForBuilding = [];
+    abliableDataForBuildingId = [];
+    if (abliableDataForStreetId[abliableDataForStreetName.indexOf($('#custStreet').val())] == undefined || stateIds[stateNames.indexOf($('#custState').val())] == undefined || abliableDataForCityesIds[abliableDataForCityesName.indexOf($('#custCity').val())] == undefined)
+        return false;
+    $.ajax({
+        type: "POST",
+        url: "/Data/GetBuildingsByCity",
+        data: { streetID: abliableDataForStreetId[abliableDataForStreetName.indexOf($('#custStreet').val())], stateID: stateIds[stateNames.indexOf($('#custState').val())], cityID: abliableDataForCityesIds[abliableDataForCityesName.indexOf($('#custCity').val())] },
+        dataType: "json",
+        success: function (response) {
+            if (response.length == 1 && response[0].BuildingNumber == null) {
+
+                abliableDataForBuildingNumber.push('');
+                abliableDataForBuildingId.push(response[0].BuildingCode);
+
+                $('#custBuldingNumber').val('');
+                //   GetBuildingsByCity();
+            }
+            if (response != null && response.length > 0 && response[0].BuildingNumber != null) {
+                $(response).each(function () {
+                    abliableDataForBuildingNumber.push(this.BuildingNumber);
+                    abliableDataForBuildingId.push(this.BuildingCode);
+                });
+            }
+            $("#custBuldingNumber").autocomplete({
+                source: abliableDataForBuildingNumber,
+            });
+            //  $('#inputBuldingNumber').val(abliableDataForBuildingNumber);
+
+            //error: function (xhr, ajaxOptions, thrownError) { alert(xhr.responseText); }
+        }
+    });
+}
+
+//Search function for customer 
+function searchCustomerData() {
+    $('#left_employee_window').empty();
+    var state = 0;
+    var city = 0;
+    //   var building = 0;
+    var street = 0;
+
+    if (stateIds[stateNames.indexOf($('#custState').val())] != undefined)
+        state = stateIds[stateNames.indexOf($('#custState').val())];
+    if (abliableDataForCityesIds[abliableDataForCityesName.indexOf($('#custCity').val())] != undefined)
+        city = abliableDataForCityesIds[abliableDataForCityesName.indexOf($('#custCity').val())];
+    if (abliableDataForStreetId[abliableDataForStreetName.indexOf($('#custStreet').val())] != undefined)
+        street = abliableDataForStreetId[abliableDataForStreetName.indexOf($('#custStreet').val())];
+
+    //if (abliableDataForBuildingId[abliableDataForBuildingNumber.indexOf($('#custBuldingNumber').val())] != undefined)
+    //    building = abliableDataForBuildingId[abliableDataForBuildingNumber.indexOf($('#custBuldingNumber').val())];
+
+
+    var data = { state: state, city: city, street: street, building: $('#custBuldingNumber').val(), custNumber: $('#custNumber').text().trim(), firstName: $('#custName').text().trim(), lastName: $('#custCompany').text().trim(), phone: $('#custPhone').text().trim(), phone1: $('#custPhone1').text().trim(), isActive: $('#isActive').is(':checked') }
+
+
+    // { manufacture: $("#ddlmanufacture").val(), lastName: $('#empLastname').val(), firstName: $('#empFirstname').val(), empNumber: $('#empNumber').val(), phoneType: $('#ddlphoneType').val(), isActive: $('#isActive').is(':checked') }
+    $.ajax({
+        type: "POST",
+        url: "/Customer/GetCustomerSarch",
+        data: data,
+        dataType: "json",
+        success: function (response) {
+            if (response == null) {
+                $('#left_employee_window').text('No records found.');
+                return true;
+            }
+            debugger;
+            var setAttr = ''
+            $(response).each(function () {
+                $('<div class="col-sm-12 col-xs-12 tab_box" onclick="selectedCustomer(this)"' +
+
+                     '" CustomerId="' + this.CustomerId + '" CustomerNumber="' + this.CustomerNumber + '" FirstName="' + this.FirstName +
+                      '" LastName="' + this.LastName + '" Floor="' + this.Floor + '" Apartment="' + this.Apartment +
+                       '" Phone2="' + this.Phone2 + '" AreaFax="' + this.AreaFax + '" Fax="' + this.Fax +
+                        '" AreaPhone2="' + this.AreaPhone2 + '" Phone1="' + this.Phone1 + '" AreaPhone1="' + this.AreaPhone1 +
+                         '" Mail="' + this.Mail + '" CustomerRemark1="' + this.CustomerRemark1 + '" CustomerRemark2="' + this.CustomerRemark2 +
+                          '" VisitInterval="' + this.VisitInterval + '" NextVisit="' + this.NextVisit + '" LastName="' + this.LastName +
+                           '" VisitDate="' + this.VisitDate + '" VisitTime="' + this.VisitTime + '" ZipCode="' + this.ZipCode +
+                       '>Company Name: '
+                   + this.FirstName + ' <p>Contact Name: ' + this.LastName + '</p><p>City: ' + this.MainAreaPhone + '-' + this.MainPhone
+                   + '</p>        </div>').appendTo($('.scroll_window'));
+                $('<div class="row" onclick="selectedEmployee(this)" EmployeeId="' + this.EmployeeId + '" EmployeeNum="' + this.EmployeeNum +
+                    '" Mail="' + this.Mail + '" FirstName="' + this.FirstName + '" LastName="' + this.LastName +
+                    '" LastSendApp="' + this.LastSendApp + '" EndDay="' + this.EndDay + '" PhoneManufactory="' + this.PhoneManufactory +
+                    '" PhoneType="' + this.PhoneType +
+                    '" MainPhone="' + this.MainPhone + '" MainAreaPhone="' + this.MainAreaPhone +
+                    '" SecondAreaPhone="' + this.SecondAreaPhone + '" SecondPhone="' + this.SecondPhone +
+                    '" StartDay="' + this.StartDay +
+
+                    '"> <div class="col-md-12 col-xs-12 tab_box">First Name: '
+                   + this.FirstName + ' <p>Last Name: ' + this.LastName + '</p><p>Phone1: ' + this.MainAreaPhone + '-' + this.MainPhone
+                   + '</p></div></div>').appendTo($('#left_employee_window'));
+            });
+
+
+        },
+        error: function (xhr, ajaxOptions, thrownError) { alert(xhr.responseText); }
+    });
+
+}
+function selectedEmployee(obj) {
+    var d = new Date();
+    d.setMonth(d.getMonth() - 3);
+    $("#datepicker1").datepicker('setDate', d);
+    d = new Date();
+    $("#datepicker2").datepicker('setDate', d);
+    var data = $(obj).attr('EmployeeId').split('|');
+    _employeeId = $(obj).attr('EmployeeId');
+    //get messgae history
+    getMessageHistory(_employeeId, $("#datepicker1 input").val(), $("#datepicker2 input").val());
+    //get Employeefill hours
+    getEmployeeTimeTemplate(_employeeId);
+    //get Employee history template
+    getEmployeeTimeHistoryDiary();
+    //Set employee data
+    setInputValues(obj);
+
+
+
+}
+
+function selectedCustomer(obj) {
+    alert('test');
+}

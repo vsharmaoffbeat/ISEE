bd89d0fae6ff52fd7030d65c9ca4886172344ac3
Info Offbeat <info@offbeatsoftwaresolutions.com>
2015-12-02 10:07:17 +0530

    Admin Changes and Report Changes

---
+++ ISEE/Scripts/Custom/Reports.js
@@ -0,0 +1,36 @@
+﻿var reportModule = angular.module('Reports', [])
+
+reportModule.controller('ReportController', function ($scope, ReportService) {
+
+    $scope.SelectedReport = null;
+
+    $scope.Reports = [
+        { text: "List Of Customer", ReportName: "ListCustomers" },
+        { text: "Customer Request", ReportName: "CustomerRequests" },
+        { text: "Employee Sms", ReportName: "EmployeeSms" }
+    ];
+
+    $scope.SelectReport = function (objReport) {
+        if (objReport == null || objReport == undefined) {
+            $scope.SelectedReport = null;
+        } else {
+            $scope.SelectedReport = objReport.ReportName;
+        }
+    }
+
+
+}).service('ReportService', function ($http) {
+    //to create unique contact id
+    var contacts = {};
+
+    contacts.GetCustomersNew = function (objData) {
+        return $http({
+            url: '/Employee/GetCustomersNew',
+            data: objData,
+            method: 'POST',
+            headers: { 'content-type': 'application/json' }
+        });
+    }
+
+    return contacts;
+});
